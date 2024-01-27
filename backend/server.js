5e98182c92fe2d5990be3e9236e256ab976fa1a0
mantelloa <adam.mantello@student.unimelb.edu.au>
2024-01-28 01:03:58 +1100

    added functions for calculating card scaling

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ app.listen(PORT, () => {
 })
 
 function shuffle(deck){
-    let new_deck = []
+    const new_deck = []
     const original_length = deck.length
     for(let i = 0; i < original_length; i++){
-        let num = getRandomInt(0, original_length - 1 - i)
+        const num = getRandomInt(0, original_length - 1 - i)
         new_deck.push(deck[num])
         deck.splice(num, 1)
     }
@@ -36,3 +36,37 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+function applyScaling(scaling, field_string, card_colour){
+    const field = removeFromField(field_string, card_colour)
+    let power_incr = 0
+    for(let i = 0; i < scaling.length; i++){
+        const match = scaling[i].match(/([A-Za-z])([+-]?\d+)/);
+        const colour = match[1]
+        const number = parseInt(match[2], 10)
+        power_incr += number*countOccurrences(field, colour)
+    }
+
+    return power_incr
+}
+
+function removeFromField(field_string, card_colour){
+    let new_field_string = field_string.split('')
+    for(let i = 0; i < card_colour.length; i++){
+        const index = new_field_string.indexOf(card_colour[i])
+        new_field_string.splice(index, 1)
+    }
+    return new_field_string
+}
+
+function countOccurrences(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(applyScaling(["B+2","C+3","A+8"], "ABBC","C"))
